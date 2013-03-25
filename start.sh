15e60010369af46900f4bed3388e1d4c52e1cce1
Jean Parpaillon <jean.parpaillon@free.fr>
2013-03-25 14:00:12 +0100

    Start reloader at startup

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ cd `dirname $0`
 export OCCI_XMPP_JID="node-1@localhost"
 export OCCI_XMMP_PASSWD="node"
 
-exec erl -pa $PWD/apps/occi/ebin $PWD/deps/*/ebin \
+exec erl -pa $PWD/apps/occi/ebin \
+		$PWD/deps/*/ebin \
 		-boot start_sasl \
+		-s reloader \
 		-s occi
