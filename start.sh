41349072d5a833cd63c5004f898cc0d50882b1cc
Jean Parpaillon <jean.parpaillon@free.fr>
2013-09-23 17:36:36 +0200

    Add config file for hello_occi app

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ export MNESIA_DIR="$HOME/.occi/"
 exec erl -pa $PWD/ebin \
     $PWD/deps/*/ebin \
     -boot start_sasl \
+    -config examples/hello_occi.config \
     -s reloader \
     -s hello_occi
