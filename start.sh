90dda639b825a08c72cf914c1aa568659002f938
Jean Parpaillon <jean.parpaillon@free.fr>
2013-10-09 18:50:52 +0200

    Add debug flag to start script

--- start.sh
+++ start.sh
@@ -1,11 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 cd `dirname $0`
-
 export MNESIA_DIR="$HOME/.occi/"
+
+tempdir=$(mktemp -d)
+config=$tempdir/hello_occi.config
+
+_exit()
+{
+    rm -rf $tempdir
+}
+
+trap _exit EXIT
+
+debug=info
+
+while getopts ":d" opt; do
+    case $opt in
+	d)
+	    debug=debug
+	    ;;
+	*)
+	    ;;
+    esac
+done
+
+cat <<EOF > $config
+[
+ {lager, [
+	  {handlers, 
+	   [
+	    {lager_console_backend, $debug}
+	   ]
+	  }
+	 ]
+ }
+].
+EOF
 
 exec erl -pa $PWD/ebin \
     $PWD/deps/*/ebin \
     -boot start_sasl \
-    -config examples/hello_occi/hello_occi.config \
+    -config $config \
+    -kernel error_logger silent \
     -s reloader \
     -s hello_occi
