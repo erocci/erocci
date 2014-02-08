bb55e6011834bb490e961f24359b9791c6c763f9
Jean Parpaillon <jean.parpaillon@free.fr>
2014-02-08 17:10:57 +0100

    DO not print curl output when testing

--- scripts/testsuite/003_create_link_networkinterface.sh
+++ scripts/testsuite/003_create_link_networkinterface.sh
@@ -31,7 +31,7 @@ for i in $(seq 1 10); do
     ]
 }
 EOF
-    ) | curl -s -f -X PUT --data @- -H 'content-type: application/json' --data @- ${occi_srv}${id}
+    ) | curl -s -f -X PUT --data @- -H 'content-type: application/json' --data @- ${occi_srv}${id} > /dev/null 2>&1
 
     if [ $? = 0 ]; then
 	echo OK
