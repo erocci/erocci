00324340067b7227fc7d7b047c470c9a6fda7a7d
leizhao <leizhao0515@gmail.com>
2014-05-14 14:28:42 +0200

    add the test of resources in occi_http_json_SUITE

--- test/occi_http_json_SUITE.erl
+++ test/occi_http_json_SUITE.erl
@@ -10,9 +10,16 @@
 -module(occi_http_json_SUITE).
 
 -compile(export_all).
+
 -compile([{parse_transform,lager_transform}]).
 
+-define(test_id, 'http://localhost:8080/myresources').
+
+-include_lib("eunit/include/eunit.hrl").
+
 -include_lib("common_test/include/ct.hrl").
+
+-include_lib("kernel/include/file.hrl").
 
 -include("occi.hrl").
 
@@ -81,7 +88,7 @@ end_per_group(_GroupName, _Config) ->
 %% Reason = term()
 %% @end
 %%--------------------------------------------------------------------
-init_per_testcase(get_resource, Config) ->
+init_per_testcase(TestCase, Config) ->
     
     Config.
 
@@ -93,7 +100,7 @@ init_per_testcase(get_resource, Config) ->
 %% Reason = term()
 %% @end
 %%--------------------------------------------------------------------
-end_per_testcase(get_resource, _Config) ->    
+end_per_testcase(TestCase, _Config) ->    
     ok.
 
 %%--------------------------------------------------------------------
@@ -110,7 +117,29 @@ end_per_testcase(get_resource, _Config) ->
 %% @end
 %%--------------------------------------------------------------------
 groups() ->
-    [].
+    [
+     {test_json,
+      [],
+      [{group,test_resource}]},
+     {test_resource,
+      [],
+      [put_resource_new,put_resource,get_resource,post_resource_new,post_resource,get_resource,delete_resource,get_resource_delete,{group,test_link}]},
+     {test_link,
+      [],
+      [put_link_new,put_link,get_link,post_link_new,post_link,get_link,delete_link,get_link,{group,test_kind}]},
+     {test_kind,
+      [],
+      [put_kind,get_kind,post_kind,get_kind,delete_kind,get_kind,{group,test_mixin}]},
+     {test_mixin,
+      [],
+      [put_mixin,get_mixin,post_mixin,get_mixin,delete_mixin,get_mixin,{group,test_query}]},
+     {test_query,
+      [],
+      [put_query,get_query,post_query,get_query,delete_query,get_query,{group,test_dir}]},
+     {test_dir,
+      [],
+      [put_dir,post_dir,delete_dir]}
+    ].
 
 %%--------------------------------------------------------------------
 %% @spec all() -> GroupsAndTestCases | {skip,Reason}
@@ -122,23 +151,34 @@ groups() ->
 %%--------------------------------------------------------------------
 all() -> 
     [
-     get_query
+     {group,test_json}
     ].   
-get_resource(_Config) ->
-    ok. 
+
 
 put_resource_new(_Config) ->
-    ok.
+    FileName=proplists:get_value(data_dir, _Config) ++ "res1.json",
+    {ok,File}=file:read_file(FileName),
+    Id = "http://localhost:8080/myresources",
+    {ok,{{_Protocol,Code,_Status},Headers,_Body}} =  httpc:request(put,{Id,[],"application/json",File},[],[]),
+    ?assertEqual(201,Code),
+    lager:info("#####PUT_RESOURCE_NEW ~p",[Headers]).
+    
+
+
 
 put_resource(_Config) ->
-    lager:info("###### Ok  "),
-    ok.
+    FileName=proplists:get_value(data_dir, _Config) ++ "res1.json",
+    {ok,File}=file:read_file(FileName),
+    Id = "http://localhost:8080/myresources",
+    {ok,{{_Protocol,Code,_Status},Headers,_Body}} =  httpc:request(put,{Id,[],"application/json",File},[],[]),
+    ?assertEqual(200,Code),
+    lager:info("#####PUT_RESOURCE ~p",[Headers]).
 
 put_link_new(_Config) ->
     ok.
 
 put_link(_Config) ->
-    ok.
+   ok.
 
 put_kind(_Config) ->
     ok.
@@ -147,16 +187,28 @@ put_mixin(_Config) ->
     ok.
 
 put_query(_Config) ->
+    
     ok.
 
 put_dir(_Config) ->
     ok.
 
 post_resource_new(_Config) ->
-    ok.
+    FileName=proplists:get_value(data_dir, _Config) ++ "res2.json",
+    {ok,File}=file:read_file(FileName),
+    Id = "http://localhost:8080/myresources",
+    {ok,{{_Protocol,Code,_Status},Headers,_Body}} =  httpc:request(post,{Id,[],"application/json",File},[],[]),
+    ?assertEqual(200,Code),
+    lager:info("#####POST_RESOURCE_NEW ~p",[Headers]).
 
 post_resource(_Config) ->
-    ok.
+    FileName=proplists:get_value(data_dir, _Config) ++ "res2.json",
+    {ok,File}=file:read_file(FileName),
+    Id = "http://localhost:8080/myresources",
+    {ok,{{_Protocol,Code,_Status},Headers,_Body}} =  httpc:request(post,{Id,[],"application/json",File},[],[]),
+    ?assertEqual(200,Code),
+    lager:info("#####POST_RESOURCE ~p",[Headers]).
+    
 
 post_link_new(_Config) ->
     ok.
@@ -177,7 +229,12 @@ post_dir(_Config) ->
     ok.
 
 delete_resource(_Config) ->
-    ok.
+    FileName=proplists:get_value(data_dir, _Config) ++ "res1.json",
+    {ok,File}=file:read_file(FileName),
+    Id = "http://localhost:8080/myresources",
+    {ok,{{_Protocol,Code,_Status},Headers,_Body}} =  httpc:request(delete,{Id,[],"application/json",File},[],[]),
+    ?assertEqual(204,Code),
+    lager:info("#####DELETE_RESOURCE  ~p",[Headers]).
 
 delete_link(_Config) ->
     ok.
@@ -189,12 +246,24 @@ delete_mixin(_Config) ->
     ok.
 
 delete_query(_Config) ->
+    
     ok.
 
 delete_dir(_Config) ->
     ok.
 
+get_resource(_Config) ->
+    Id = "http://localhost:8080/myresources",
+    {ok, {{_Protocol,Code,_Status}, Headers, _Body}} = httpc:request(get, {Id, [{"accept","text/plain"}]}, [], []),   
+    lager:info("#### GET_RESOURCE:::~p",[Headers]),
+    ?assertEqual(200, Code),
+    ?assert(lists:member({"location", Id}, Headers)).
 
+get_resource_delete(_Config) ->
+    Id = "http://localhost:8080/myresources",
+    {ok, {{_Protocol,Code,_Status}, Headers, _Body}} = httpc:request(get, {Id, [{"accept","text/plain"}]}, [], []),   
+    lager:info("#### GET_RESOURCE_DELETE:::~p",[Headers]),
+    ?assertEqual(404, Code).
 
 get_link(_Config) ->
     ok.
@@ -206,6 +275,8 @@ get_mixin(_Config) ->
     ok.
 
 get_query(_Config) ->
-    ?assertMatch({ok,{{_Protocol,200,_Status},Headers,_Body}},
-		 httpc:request(get,{"http/::localhost:8080/-/",[{"accept","application/json",{},[],[]}]})),
-    ?assert(lists:member({"content-type","application/json"},Headers)).
+    {ok,{{_Protocol, Code, _Status}, Headers, _Body}} = httpc:request(get,{"http://localhost:8080/-/",[{"accept","application/json"}]},[],[]),
+    ?assertEqual(200,Code),
+    ?assert(lists:member({"content-type","application/json"}, Headers)).
+
+
